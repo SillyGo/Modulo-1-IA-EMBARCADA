785f496f4a3d5fe8faf62b77c031d371eba2822b
SillyGo <acvmg@cin.ufpe.br>
2025-08-24 06:24:49 -0300

    write

--- other_codes/test1.cpp
+++ other_codes/test1.cpp
@@ -1,1 +1,16 @@
 #include <arduino.h>
+
+void setup()
+{
+    Serial.begin(115200);
+}
+
+void loop()
+{
+    #ifdef ESP32
+      Serial.print("Temperature: ");
+      Serial.print(temperatureRead());
+      Serial.println(" ºC");
+    #endif
+    delay(2000);
+}
--- other_codes/test2.cpp
+++ other_codes/test2.cpp
@@ -1,2 +1,58 @@
 #include <arduino.h>
 
+TaskHandle_t Task1;
+TaskHandle_t Task2;
+
+void task1(void *pvParameters)
+{
+  while (1)
+  {
+    #ifdef ESP32
+      Serial.print("Temperature: ");
+      Serial.print(temperatureRead());
+      Serial.println(" ºC");
+    #endif
+    delay(1000);
+  }
+}
+
+void task2(void *pvParameters)
+{
+    digitalWrite(2, HIGH);
+    delay(200);
+    digitalWrite(2,LOW);
+    delay(200);
+}
+
+void setup()
+{
+    Serial.begin(115200);
+    xTaskCreatePinnedToCore(
+    task1,
+    "Task1",
+    10000,
+    NULL,
+    1,
+    &Task1,
+    0
+  );
+  xTaskCreatePinnedToCore(
+    task2,
+    "Task2",
+    10000,
+    NULL,
+    1,
+    &Task2,
+    1
+  );
+  pinMode(2, OUTPUT);
+}
+
+void loop()
+{
+    int x = 10;
+    int y = 15;
+    int z = x * y;
+    x++;
+    y++;
+}
